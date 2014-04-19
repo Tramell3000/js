b35260670945785d99e824e4971cad1ca84051ff
TramellCutler <TramellCutler@fullsail.edu>
2014-04-18 20:44:46 -0400

    commit
    change in commit

---
+++ script.js
@@ -0,0 +1,14 @@
+	var cash = 100000;
+var car = 60000;
+
+
+
+if (cash > car){
+	console.log("You can purchase");
+
+prompt("You can purchase");
+
+
+}else{
+	console.log("You will not be able to purchase");
+} 
